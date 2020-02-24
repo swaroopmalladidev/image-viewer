50b5af7524b6e22fc3afab644bcc0cfbf5672809
swaroopmalladidev <swaroop.malladi@gmail.com>
2020-02-24 08:45:12 +0530

    Added Search box and Profile on Home Page header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom';
 import './Header.css';
+import Input from '@material-ui/core/Input';
+import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
 
 class Header extends Component {
 
@@ -11,6 +14,13 @@ class Header extends Component {
         }
     }
 
+    searchBoxChangeHandler = () => {
+
+    }
+
+    profilePictureClickHandler = () => {
+
+    }
     
     render() {
         const {open} = this.state;
@@ -18,6 +28,23 @@ class Header extends Component {
             <div className='app-header'>
                 {!this.state.isUserLoggedIn && <Redirect to='/' />}
                 <Link to="/home" className="app-logo">Image Viewer</Link>
+                {this.state.isUserLoggedIn &&
+                    <div className="header-right-area">
+                        
+                        {this.props.pageId === 'home' &&
+                            <div className="searchBox-headerarea">
+                                <SearchIcon className="searchIcon" />
+                                <Input placeholder="Search..." disableUnderline={true} className="searchBox" onChange={this.searchBoxChangeHandler} />
+                            </div>
+                        }
+                        
+                        <div>
+                            <IconButton className="profile-pictureIcon" onClick={this.profilePictureClickHandler}>
+                            <img src={this.props.profilePicture} alt="Profile Pic" className="profile-pic" />
+                            </IconButton>
+                        </div>
+                    </div>
+                }
             </div>
         )
     }
